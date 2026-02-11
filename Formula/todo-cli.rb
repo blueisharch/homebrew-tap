537ebee664e76828a08c655daa9c15b107aeb403
Blueprint Architect <gourangadassamrat13@gmail.com>
2026-02-11 21:35:03 +0600

    chore: update todo-cli formula to v1.1.0

--- Formula/todo-cli.rb
+++ Formula/todo-cli.rb
@@ -1,8 +1,8 @@
 class TodoCli < Formula
   desc "A simple Todo CLI built in Go"
   homepage "https://github.com/blueisharch/todo-cli-go"
-  url "https://github.com/blueisharch/todo-cli-go/releases/download/v1.0.5/todo-v1.0.5-darwin-amd64.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256" # The action will update this automatically later
+  url "https://github.com/blueisharch/todo-cli-go/releases/download/v1.1.0/todo-v1.1.0-darwin-amd64.tar.gz"
+  sha256 "13b379b77ec49c84764c2fa653518c2dab80059ac3be1a21f9bf167e75dc9d6c" # The action will update this automatically later
   license "MIT"
 
   def install
